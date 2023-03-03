b2fcc9023ab5c64823ff0127784545de283377bf
Maazin Zubair <mzuba002@xe-01.cs.ucr.edu>
2023-03-02 22:39:36 -0800

    added combatClass branch, added .h file and basic .cpp file for it

---
+++ source/CombatEV.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "../header/combatev.h"
+
+void CombatEV::runEvent(Entity player, Enemy enemy) {
+    /*
+        if (chckAgl(player,enemy))
+            fight(player,enemy);
+        else
+            fight(enemy,player);
+    */
+}
+bool CombatEV::chckAgl(Entity player, Enemy enemy) {
+    /*
+        if (player's agility is > enemies agility)
+            return true
+        return false; player is slower than enemy
+    */
+}
+void CombatEV::fight(Entity first, Enemy second) {
+    /*
+        bool fight = true;
+        int dmg = 0;
+        while (fight) {
+            first.doCombat();
+            if (dmg > 0)
+                second.setHP(dmg);
+
+        }
+    
+    
+    */
+}
