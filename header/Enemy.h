01a63cdf210c70066ee7f95d328b4f5d804fb49f
Maazin Zubair <mzuba002@xe-01.cs.ucr.edu>
2023-03-02 21:46:34 -0800

    removed unnessesary files, split enemy into enemyData and enemyStats, and dded code for all the h files and only enemystats.cpp

--- header/Enemy.h
+++ header/Enemy.h
@@ -6,40 +6,19 @@
 
 using namespace std;
 
-class Enemy : public EntityStats {
-
+class Enemy : public EnemyStats, public EnemyData {
+    private:
+        Item loot;
 
     public:
-        enum EnemyLootDrops {
-            Implant,            //should be changed to specific implant name when we figure that out"
-            StatUpgrade_Vitality,
-            StatUpgrade_Agility,
-            StatUpgrade_Strength,
-            StatUpgrade_Compatibility,
-            StatUpgrade_Luck,
-            //etc (any other loot drop we would want an enemy to hold)
-        };
-
-        //constructor
-        Enemy(int _vitality, int _strength, int _agility, int _compatibility, int _luck, EnemyLootDrops _LD, double _xpDrop) : EntityStats(_vitality, _strength, _agility, _compatibility, _luck){
-            this->LD = _LD;
-            this->dead = false;
-            this->xpDrop = max(_xpDrop, 0.0);
+        Enemy(string enemyName, int _vitality, int _agility, int _luck, float startingHealth, float xpDrop) : EnemyStats(_vitality, _agility, _luck){
+            setName(enemyName);
+            SetMaxHealth(startingHealth);
+            setExp(xpDrop);
         }
 
-        //getters and setters
-        void setxpDrop(double xp);
-        double getxpDrop();
-        void updateDeathStatus();
-        bool getDeathStatus();
-        void setEnemyLootDrop(EnemyLootDrops lootdp);
-        EnemyLootDrops getEnemyLootDrop();
-
-    private:
-        EnemyLootDrops LD;
-        bool dead;
-        double xpDrop;
-
+        void equipWeapon(string weaponName, float weaponDMG, float weaponCrit);
+        Item getDrop();
 };
 
 
--- source/Enemy.cpp
+++ source/Enemy.cpp
@@ -2,26 +2,4 @@
 
 using namespace std;
 
-void Enemy::setxpDrop(double xp){
-    this->xpDrop = max(xp, 0.0);
-}
 
-double Enemy::getxpDrop(){
-    return this->xpDrop;
-}
-
-void Enemy::updateDeathStatus(){
-    this->dead = true;
-}
-
-bool Enemy::getDeathStatus(){
-    return this->dead;
-}
-
-void Enemy::setEnemyLootDrop(EnemyLootDrops lootdp){
-    this->LD = lootdp;
-}
-
-Enemy::EnemyLootDrops Enemy::getEnemyLootDrop(){
-    return this->LD;
-}
