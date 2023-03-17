77072a3da76da5cb462647b7f0d9f7bbe427ff8f
Javier Vargas <jvarg123@xe-01.cs.ucr.edu>
2023-03-17 13:52:15 -0700

    Phase 4 doc

--- source/main.cpp
+++ source/main.cpp
@@ -16,5 +16,30 @@ int main() {
     }
     delete menu;
 
+    /*
+        ADD_EXECUTABLE(runPrgm
+	source/main.cpp
+	source/Game.cpp
+    source/LootEv.cpp
+    source/Item.cpp
+    source/Menu.cpp
+    source/Story.cpp
+    source/Weapon.cpp
+    source/Inventory.cpp
+    source/EnemyStats.cpp
+    source/EnemyData.cpp
+    source/Enemy.cpp
+    source/CreateChar.cpp
+    source/Consumable.cpp
+    source/CombatEV.cpp
+    source/Combat.cpp
+    source/CharacterStats.cpp
+    source/CharacterData.cpp
+    source/Character.cpp
+)
+
+    
+    */
+
     return 0;
 }
