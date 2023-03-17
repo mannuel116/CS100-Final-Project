757755a23ebaa363c59363ff1a30190c63579bbd
mannuel116 <63628421+mannuel116@users.noreply.github.com>
2023-03-17 13:13:16 -0700

    committed WeaponTest.cpp

---
+++ test/WeaponTest.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "../header/Weapon.h"
+#include <iostream>
+
+using namespace std;
+
+TEST(WeaponTest, DamageTest){
+    Weapon *weapon = new Weapon("name", "descrp", 20, 10);
+    EXPECT_EQ(weapon->Damage(),20);
+    delete weapon;
+}
+
+TEST(WeaponTest, CritTest){
+    Weapon *weapon = new Weapon("name", "descrp", 20, 10);
+    EXPECT_EQ(weapon->Crit(),10);
+    delete weapon;
+}
