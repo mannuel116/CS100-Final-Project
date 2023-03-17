3293526884058456fb0e06c07628027c94aa0219
jvarCS <107980661+jvarCS@users.noreply.github.com>
2023-03-17 10:44:11 -0700

    Update Inventory.h

--- header/Inventory.h
+++ header/Inventory.h
@@ -7,6 +7,25 @@
 #include "Character.h"
 #include <vector>
 
+class Inventory {
+    private:
+        vector<Consumable *> consumables;
+        vector<Weapon *> weapons;
+        int invSize;
+        int itemCnt;
+    public:
+        Inventory();
+        void invMenu(Character *);
+        void setInvSize(int);
+        void getWep(int, Character *);
+        void printCons(Character *);
+        void printWep(Character *);
+        void useCons(int, Character *);
+        void equipWep(int, Character *);
+        void addWep(Weapon *);
+        void addCons(Consumable *);
+        bool full();
+        ~Inventory();
 };
 
 #endif
